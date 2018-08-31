f0f48b410a8f29798e6eef3e4e6a9899c13ed613
Andreas Stenlund <a.stenlund@gmail.com>
2018-09-01 00:15:36 +0200

    Initial commit

---
+++ Convert2Kindle/Program.cs
@@ -0,0 +1,158 @@
+﻿namespace Convert2Kindle
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                var result = WalkDirectory(Environment.CurrentDirectory, new List<string>());
+                var success = result.Item1;
+                var convertedCount = result.Item2;
+
+                if (success)
+                {
+                    if (convertedCount > 1)
+                    {
+                        SayWithColor($"\nAll done, {convertedCount} books were converted\n", ConsoleColor.Green);
+                    }
+                    else if (convertedCount == 1)
+                    {
+                        SayWithColor("\nAll done, one book was converted\n", ConsoleColor.Green);
+                    }
+                    else
+                    {
+                        SayWithColor("\nThere were no books to convert\n", ConsoleColor.Green);
+                    }
+                }
+                else
+                {
+                    SayWithColor("\nThere were errors converting one or more books\n", ConsoleColor.Red);
+                    Environment.ExitCode = 1;
+                }
+            }
+            catch (Exception e)
+            {
+                SayWithColor($"\nException caught: {e}\n", ConsoleColor.Red);
+                Environment.ExitCode = 1;
+            }
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey();
+        }
+
+        private static Tuple<bool, int> WalkDirectory(string dir, List<string> visitedDirs)
+        {
+            var success = true;
+            var convertedCount = 0;
+
+            if (visitedDirs.Contains(dir))
+            {
+                Console.WriteLine("Recursive path found: \"{0}\", returning...", dir);
+                return new Tuple<bool, int>(true, 0);
+            }
+
+            visitedDirs.Add(dir);
+
+            foreach (var input in Directory.GetFiles(dir).Where(file => Path.GetExtension(file) == ".epub"))
+            {
+                var output = Path.ChangeExtension(input, ".mobi");
+
+                if (File.Exists(output))
+                {
+                    Console.WriteLine("Book already converted: \"{0}\", skipping...", Path.GetFileNameWithoutExtension(input));
+                    continue;
+                }
+
+                if (Convert(input, output))
+                {
+                    ++convertedCount;
+                }
+                else
+                {
+                    success = false;
+                    Console.Write("Press any key to continue . . . ");
+                    Console.ReadKey();
+                }
+            }
+
+            foreach (var subdir in Directory.GetDirectories(dir))
+            {
+                var result = WalkDirectory(subdir, visitedDirs);
+                success &= result.Item1;
+                convertedCount += result.Item2;
+            }
+
+            return new Tuple<bool, int>(success, convertedCount);
+        }
+
+        private static bool Convert(string input, string output)
+        {
+            output = Path.GetFileName(output);
+
+            var startInfo = new ProcessStartInfo
+                {
+                    FileName = "kindlegen.exe",
+                    Arguments = $"\"{input}\" -o \"{output}\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                };
+
+            var proc = new Process
+                {
+                    StartInfo = startInfo
+                };
+
+            Console.WriteLine("Converting with command: {0} {1}", startInfo.FileName, startInfo.Arguments);
+
+            proc.Start();
+
+            var stdOut = proc.StandardOutput.ReadToEnd();
+            
+            proc.WaitForExit();
+
+            var bookName = Path.GetFileNameWithoutExtension(input) ?? Path.GetFileName(input) ?? input;
+            var result = true;
+            
+            switch (proc.ExitCode)
+            {
+                case 0:
+                    SayWithColor($"\nConversion of \"{bookName}\" finished successfully\n", ConsoleColor.Green);
+                    break;
+                case 1:
+                    SayWithColor($"\nConversion of \"{bookName}\" finished with warnings\n", ConsoleColor.Yellow);
+                    break;
+                case 2:
+                default:
+                    SayWithColor($"\nConversion of \"{bookName}\" failed\n", ConsoleColor.Red);
+                    WriteLog(Path.ChangeExtension(input, ".log"), stdOut);
+                    result = false;
+                    break;
+            }
+
+            return result;
+        }
+
+        private static void SayWithColor(string message, ConsoleColor color)
+        {
+            var consoleForegroundColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = consoleForegroundColor;
+        }
+
+        private static void WriteLog(string path, string contents)
+        {
+            if (contents.Length > 0)
+            {
+                File.WriteAllText(path, contents);
+            }
+        }
+    }
+}
---
+++ Convert2Kindle/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Convert2Kindle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Convert2Kindle")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c2407ce9-1ad5-4203-80c7-82b4d4509ac8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
